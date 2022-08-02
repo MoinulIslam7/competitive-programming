f854a7ae754e33f601f62f2e0ee68fd65aa28295
Moinul Islam <92529420+MoinulIslam7@users.noreply.github.com>
2022-08-03 00:42:35 +0600

    Add files via upload

---
+++ URI ONLINE JUDGE/brazilworldcup.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(scanf("%d",&n) != EOF)
+    {
+        if(n==0)
+            cout<<"vai ter copa!\n";
+        else
+            cout << "vai ter duas!\n";
+    }
+}
